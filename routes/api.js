46b7b00d5448cb379c5a4458a8af0aa8320a0101
Slynax <remlouplay3@gmail.com>
2024-07-18 15:58:00 +0200

    fix route

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const {sendDataToClient} = require('../webSocket');
 
-router.post('/sendTo', asyncHandler(async (req, res) => {
+router.post('/rooms/:roomId/messages/send', asyncHandler(async (req, res) => {
     const { data } = req.body;
+    const { roomId } = req.params;
     const token = req.headers['authorization'];
+    data.IRoom._id = roomId;
     sendDataToClient(token, data);
     res.status(200).json(response);
 }));
